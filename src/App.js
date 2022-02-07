02f01310b494ef53025ee7ce7959b0f6ca49fed8
SeanKurtz97 <seankurtz75@gmail.com>
2022-02-07 07:14:00 -0600

    Updated fetchto new api url

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
       const query_str = qs.stringify({ genre: searchInput.query });
       try {
         const results = await axios.get(
-          `https://genre-detector-backend-340610.uc.r.appspot.com/genres?${query_str}`
+          `https://api.genredetector.com/genres?${query_str}`
         );
         if (results.data.status === 200) {
           setSearchResults(results.data.msg);
@@ -59,7 +59,7 @@ function App() {
       const query_str = qs.stringify({ name: searchInput.query });
       try {
         const results = await axios.get(
-          `https://genre-detector-backend-340610.uc.r.appspot.com/artists?${query_str}`
+          `https://api.genredetector.com/artists?${query_str}`
         );
         if (results.data.status === 200) {
           setSearchResults(results.data.msg);
