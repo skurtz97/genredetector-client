6ac3f976cbb0f45d00bfdc2c4f87bba96d9c0faf
skurtz97 <seankurtz75@gmail.com>
2022-03-14 16:03:08 -0500

    Added test text for new header.

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,7 @@ export function Header() {
     return (
         <header className={styles.header}>
             <h1>Genre Detector</h1>
+            <p>This is a test</p>
             <p>Please like and subscribe to friends to the end</p>
         </header>
     )
